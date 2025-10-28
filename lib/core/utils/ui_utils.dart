eed52209fd3d3f6e60421fbfc70249860fe72287
Islam Ramzy <islamramzy2@gmail.com>
2025-10-29 00:11:07 +0300

    completing the basic configuration needed for the develop branch

--- lib/core/utils/ui_utils.dart
+++ lib/core/utils/ui_utils.dart
@@ -1,5 +1,8 @@
+import 'package:exam_app/core/resources/color_managar.dart';
+import 'package:exam_app/core/resources/styles_manager.dart';
 import 'package:exam_app/core/widgets/loading_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
 class UIUtils {
@@ -24,4 +27,35 @@ class UIUtils {
 
   static void showMessage(String message) =>
       Fluttertoast.showToast(msg: message, toastLength: Toast.LENGTH_SHORT);
+  static void showMessageWithNav({required BuildContext context,String? title,required String message,String? posActionName,Function? posAction,String? negActionName,Function? negAction}){
+    List<Widget>? actions=[];
+    if(posActionName!=null){
+      actions.add(TextButton(
+        onPressed: () {
+          Navigator.pop(context);
+          posAction?.call();
+        }, 
+        child: Text(posActionName,style: getMediumStyle(color: ColorManager.black,fontSize: 18.sp),)
+      )
+      );
+    }
+    if(negActionName!=null){
+      actions.add(TextButton(
+        onPressed: () {
+          Navigator.pop(context);
+          negAction?.call();
+        }, 
+        child: Text(negActionName,style: getMediumStyle(color: ColorManager.black,fontSize: 18.sp),)
+      )
+      );
+    }
+    showDialog(
+      context: context, 
+      builder: (context) => AlertDialog(
+        title: Text(title?? '',style: getMediumStyle(color: ColorManager.black,fontSize: 18.sp),),
+        content: Text(message,style: getMediumStyle(color: ColorManager.black,fontSize: 18.sp),),
+        actions: actions,
+      ),
+    );
+  }
 }
--- lib/features/Auth/data/datasources/local/shared_prefs_data_source.dart
+++
@@ -1,5 +0,0 @@
- import 'package:exam_app/features/Auth/data/datasources/local/auth_local_data_source.dart';
-
-class SharedPrefsDataSource implements AuthLocalDataSource {
-  
-}
