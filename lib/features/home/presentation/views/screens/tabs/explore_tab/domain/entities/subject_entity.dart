86f460f67c22b6bba0329507e4f38967ee3e67f0
Islam Ramzy <islamramzy2@gmail.com>
2025-11-17 23:51:54 +0200

    finishing api, domain, data layers of explore tab

---
+++ lib/features/home/presentation/views/screens/tabs/explore_tab/domain/entities/subject_entity.dart
@@ -0,0 +1,6 @@
+class SubjectEntity {
+  String? id;
+  String? name;
+  String? icon;
+  SubjectEntity({this.id,this.name,this.icon});
+}
---
+++ lib/features/home/presentation/views/screens/tabs/explore_tab/domain/repositories/explore_tab_repository.dart
@@ -0,0 +1,6 @@
+import 'package:exam_app/config/base_response/base_response.dart';
+import 'package:exam_app/features/home/presentation/views/screens/tabs/explore_tab/domain/entities/subject_entity.dart';
+
+abstract class ExploreTabRepository {
+  Future<BaseResponse<List<SubjectEntity>>> getAllSubjects(String token);
+}
