fcc7297aad2d6a9edf28d688e89d7392bc8a4dfa
Ahmed Yahia <ahmed22y10@gmail.com>
2025-12-11 22:35:35 +0200

    Update_Structure

--- lib/features/profile_tab/domain/repositories/edit_profile_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:exam_app/config/base_response/base_response.dart';
-import 'package:exam_app/features/profile_tab/data/models/edit_profile_request.dart';
-import 'package:exam_app/features/profile_tab/data/models/edit_profile_response/edit_profile_response.dart';
-
-abstract class EditProfileRepository {
-  Future<BaseResponse<EditProfileResponse>> editProfile(
-    EditProfileRequest request,
-  );
-}
