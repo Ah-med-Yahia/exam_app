55532b3418d9811b2f48c2fca1bf77abbf482e79
Islam Ramzy <islamramzy2@gmail.com>
2025-12-15 22:34:20 +0200

    fixing app initial route in main file

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class ExamApp extends StatelessWidget {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
           onGenerateRoute: RouteGenerator.getRoute,
-          initialRoute: Routes.home,
+          initialRoute: Routes.signUp,
           theme: AppTheme.lightTheme,
           themeMode: ThemeMode.light,
           builder: EasyLoading.init(),
